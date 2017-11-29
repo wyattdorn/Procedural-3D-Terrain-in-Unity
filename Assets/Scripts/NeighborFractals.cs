359fff7abc0748b93227f88eccd3e96470ecbcb6
wyattdorn <wyatt.dorn@gmail.com>
2017-11-28 22:48:01 -0800

    Beginnings of infinite scrolling terrain
    -Added scripts that will (soon) allow new terrain objects to be created programatically, with edges that seemlessly match their partners.

---
+++ Assets/Scripts/NeighborFractals.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// The Following is an implementation of the Diamond-Square fractal algorithm.
+/// 
+/// </summary>
+
+public class NeighborFractals : MonoBehaviour {
+
+	System.Random randNum;
+	float[,] heightmap;
+	float[,] heights;
+	TerrainData td;
+	int hash;
+	int fractalSize;
+	int size_x;
+	float roughness;
+
+	public void generateFractal(ref float[,] myHeights, int size, float rough){
+		roughness = rough;
+		fractalSize = size * 2;
+		heightmap = new float[fractalSize,fractalSize];
+		for (int x = 0; x < fractalSize; x++) {
+			for (int y = 0; y < fractalSize; y++) {
+				heightmap [x, y] = 0.5f;
+			}
+		}
+		returnTerrain (ref myHeights, ref heightmap, size);
+	}
+
+	void returnTerrain(ref float[,] myHeights, ref float[,] heightmap, int size){
+		randNum = new System.Random();
+		size_x = fractalSize - 1;
+		halve (size_x);
+		for (int x = 0; x < size; x++) {
+			for (int y = 0; y < size; y++) {
+				myHeights [x, y] = heightmap [x + 512, y + 512];
+			}
+		}
+	}
+
+	void halve(int fractalSize){
+		int half = fractalSize / 2;
+		if (half < 1) {
+			//We've reached the last itteration of the loop
+			return;
+		}
+		for (int y = half; y < size_x; y += fractalSize) {
+			for (int x = half; x < size_x; x += fractalSize) {
+				square(x, y, half, ((float)randNum.NextDouble() - 0.5f)/(size_x/fractalSize));
+			}
+		}
+		for (int y = 0; y <= size_x; y += half) {
+			for (int x = (y + half) % fractalSize; x <= size_x; x += fractalSize) {
+				diamond(x, y, half, ((float)randNum.NextDouble() - 0.5f)/(size_x/fractalSize));
+			}
+		}
+		halve(half);
+	}
+		
+	void diamond(int x, int y, int fractalSize, float offset){
+		if (heightmap [x, y] != 0.5) {
+			return;
+		}
+		float average = avg(new float[4]{	isVAlidPoint(x, y - fractalSize),
+											isVAlidPoint(x + fractalSize, y),
+											isVAlidPoint(x, y + fractalSize),
+											isVAlidPoint(x - fractalSize, y)});
+		heightmap[x, y] = average + offset * roughness;
+	}
+
+	void square(int x, int y, int fractalSize, float offset){
+		if (heightmap [x, y] != 0.5) {
+			return;
+		}
+		float average = avg(new float[4]{	isVAlidPoint (x - fractalSize, y - fractalSize),
+											isVAlidPoint (x + fractalSize, y - fractalSize),
+											isVAlidPoint (x + fractalSize, y + fractalSize),
+											isVAlidPoint (x - fractalSize, y + fractalSize)});
+		heightmap [x, y] = average + offset * roughness;
+	}
+		
+	float isVAlidPoint(int x, int y){
+		if (x < 0 || x > size_x || y < 0 || y > size_x) {
+			return 0.0f;
+		}
+		return heightmap[x, y];
+	}
+
+	float avg(float[] values){
+		float total = 0f;
+		for (int x = 0; x < 4; x++) {
+			total += values [x];
+		}
+		return total / 4.0f;
+	}
+}
---
+++ Assets/Scripts/NeighborTerrainScript.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeighborTerrainScript : MonoBehaviour {
+
+	[Range(0.1f, 1f)]
+	public float rough = 0.5f;
+
+	System.Random rng;
+	float[,] heights, tempHeights;
+	Terrain myTerrain;
+	TerrainData td;
+	int hash, size_x, fractalSize, max;
+	[Range(0, 25)]
+
+	float[, ,] splatmapData;
+
+	public Rigidbody FPSCam;
+
+	// What runs every time we press play
+	void Start () {
+		initializeAll ();
+		myTerrain.terrainData.SetDetailResolution (1000, 1000);
+		generateTerrain ();
+		FPSCam.position = (new Vector3 (500, heights[500,500]+100f, 500));
+	}
+
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Return) || Input.GetKeyUp (KeyCode.Mouse0)) {
+			Start ();
+		}
+	}
+
+	void generateTerrain(){
+		splatmapData = new float[myTerrain.terrainData.alphamapResolution, myTerrain.terrainData.alphamapResolution, myTerrain.terrainData.alphamapLayers];
+
+		Fractals fractalCode = gameObject.AddComponent <Fractals>();
+
+		fractalCode.generateFractal (ref heights, size_x, 3.3f);
+
+		normalizeHeights();
+
+
+
+		matchNeighbor ();
+
+		Debug.Log (heights[0,0]+", "+heights[100,100]);
+
+		myTerrain.terrainData.SetHeights (0, 0, heights);
+		myTerrain.terrainData.SetAlphamaps(0, 0, splatmapData);
+
+		//make sure to get rid of heights[,] and only use tempHeights[,]
+		//also, might want to change the roughness sent to fractal method
+	}
+
+	void initializeAll(){
+		size_x = 1025;
+		myTerrain = GetComponent<Terrain>();
+		td = myTerrain.terrainData;
+
+		heights = new float[size_x,size_x];
+		tempHeights = new float[size_x,size_x];
+	}
+		
+	bool isValidPoint(int x, int z){
+		if (x >= 0 && x < size_x) {
+			if (z >= 0 && z < size_x) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void matchNeighbor(){
+		Debug.Log ("match!");
+		for (int x = 0; x < size_x; x++) {
+			for (int y = 0; y < size_x; y++) {
+				heights [x, y] = 1.0f;
+			}
+		}
+	}
+
+	void normalizeHeights(){
+		float lowest, highest;
+		highest = lowest = 0.5f;
+		for (int x = 0; x < size_x; x++) {
+			for (int y = 0; y < size_x; y++) {
+				if (heights [x, y] <= lowest) {
+					lowest = heights [x, y];
+				}
+				else if(heights [x, y] >= highest) {
+					highest = heights [x, y];
+				}
+			}
+		}
+
+		highest = highest - lowest;
+
+		for (int x = 0; x < size_x; x++) {
+			for (int y = 0; y < size_x; y++) {
+				heights [x, y] -= lowest;
+				heights [x, y] /= highest;
+				heights [x, y] *= heights [x, y];
+			}
+		}
+
+	}
+		
+}
